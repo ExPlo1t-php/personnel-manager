bd0d716b66b7c442cbdeab885480e98eb9d715b1
slayer <younes_tm@outlook.com>
2022-02-12 15:41:04 +0100

    first commit

---
+++ employees.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-free-6.0.0-web/css/all.css">
+    <link rel="stylesheet" href="table.css">
+    <title>Gestionnaire Du Personnel | Employees</title>
+</head>
+<body class="w-100 bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Gestionnaire</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse d-flex justify-content-around " id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto ">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home  </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search.php">Recherche </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="employees.php">employees<span class="sr-only">(actuel)</span></a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li> -->
+                    
+                </ul>
+
+        </div>
+    </nav>
+    <main class="container">
+        <div>
+            <table class="table table-bordered table-light">
+                <thead >
+                    <tr>
+                        <th>Image</th>
+                        <th>Matricule</th>
+                        <th>Nom</th>
+                        <th>Prenom</th>
+                        <th>Date <br> de naissance</th>
+                        <th> Departement</th>
+                        <th>Salaire</th>
+                        <th>Fonction</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td><img class="pimage" src="assets/images/george.jpeg" ></td>
+                        <td>Matricule</td>
+                        <td>Nom</td>
+                        <td>Prenom</td>
+                        <td>Date <br> de naissance</td>
+                        <td> Departement</td>
+                        <td>Salaire</td>
+                        <td>Fonction</td>
+                        <td><button><i class="fa-solid fa-pen"></i></button><button><i class="fa-solid fa-trash-can"></i></button></td>
+                    </tr>
+                    <tr>
+                        <td><img class="pimage" src="assets/images/george.jpeg" ></td>
+                        <td>Matricule</td>
+                        <td>Nom</td>
+                        <td>Prenom</td>
+                        <td>Date <br> de naissance</td>
+                        <td> Departement</td>
+                        <td>Salaire</td>
+                        <td>Fonction</td>
+                        <td><button><i class="fa-solid fa-pen"></i></button><button><i class="fa-solid fa-trash-can"></i></button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
